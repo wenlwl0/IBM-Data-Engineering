14534b68efbf664b762b94880a06824c8ba20f1c
wenlwl0 <leewenli@gmail.com>
2026-07-30 03:46:30 -0400

    Linux Informational Commands

---
+++ 06-Linux-Labs/linux_lab_commands.sh
@@ -0,0 +1,17 @@
+    1  whoami #returns current username
+    2  uname #basic info about OS
+    3  uname -a #-a prints all system info
+    4  id #displays userid & groupid of user
+    5  df #available disk space
+    6  df -h #available disk space in human-readable format
+    7  ps #lists each process currently running and its process id
+    8  ps -e #-e displays all processes running on system including those owned by other users
+    9  top #details on processes/threads currently running & managed by kernel
+   10  top -n 10 #exit after x no. of repetitions
+   11  echo "Hi"  #displays given text on screen
+   12  echo -e "This prints \nin two lines"
+   13  date #current date and time
+   14  date "+%D"
+   15  man #user manual for any command provided as its argument
+   16  man ls #for manual page for ls command
+   17  history 50 > linux_lab_commands.sh
